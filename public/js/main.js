e4bee338783221cc8004316f0f3cf3bcb148d74c
VitorLucas44 <vitoriosolucas@hotmail.com>
2022-12-21 11:33:48 +0100

    finis

---
+++ public/js/main.js
@@ -0,0 +1,87 @@
+// exo1
+
+// const numbers = [1, 2, 3, 4, 5, 6, 7 , 8, 9, 10];
+// for (const number of numbers) {
+//   console.log(number);
+// }
+
+
+
+// exo2
+
+// let boucle = 10;
+// while (boucle > 0) {
+// console.log(boucle);
+// boucle--;
+// }
+
+
+// exo3
+
+// let paire = 2;
+// do {
+//     console.log(paire);
+//     paire += 2;
+// } while (paire <= 20);
+
+
+
+
+// exo4
+
+// const mots = ['un', 'feux', 'trois'];
+// for (const mots of mots) {
+//   console.log(mots);
+// }
+
+
+
+
+
+// exo5
+
+// let calc = 0;
+// for (let i = 1; i <= 10; i++) {
+// calc += i;
+// }
+// console.log(sum);
+
+
+
+
+// exo6
+
+// let num = 0;
+// while (num <= 0) {
+// num = parseInt(prompt("Saisissez un nombre positif :"));
+// }
+// console.log("Vous avez saisi un nombre positif : " + num);
+
+
+
+// exo7
+// for (let i = 1; i <= 10; i++) {
+//     console.log(i);
+//     if (i === 5) {
+//     break;
+//     }
+// }
+
+
+
+
+
+// exo8
+
+for (let i = 1; i <= 10; i++) {
+    if (i + 2 === 0) {
+    continue;
+    }
+console.log(i);
+}
+
+
+
+
+
+
